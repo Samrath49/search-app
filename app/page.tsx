525617300f9ec36bcb5793adfcbbce9bcee2ab1f
samrath49 <samrathchauhan49@gmail.com>
2024-12-16 23:05:59 +0530

    feat: Home page changes to make use of GoogleSearch component.

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,15 @@
 import Image from "next/image";
 import Footer from "./_components/footer";
 import Header from "./_components/header";
-import { Button } from "@/components/ui/button";
+import LanguageSupport from "./_components/language-support";
+import { GoogleSearch } from "./_components/google-search";
 
 export default function Home() {
   return (
     <div className="flex flex-col relative min-h-screen">
       <Header />
       <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center p-8 pb-20 gap-16 sm:p-20">
-        <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
+        <main className="flex flex-col gap-8 row-start-3 items-center sm:items-start">
           <Image
             src="/logo.png"
             alt="Search logo"
@@ -17,16 +18,8 @@ export default function Home() {
             priority
           />
         </main>
-      </div>
-      <div className="flex gap-4 w-full justify-center">
-        <Button variant="secondary">Google Search</Button>
-        <a
-          className={`btn btn-secondary`}
-          target="_blank"
-          href="https://doodles.google/"
-        >
-          <Button variant="secondary">{`I'm Feeling Lucky`}</Button>
-        </a>
+        <GoogleSearch />
+        <LanguageSupport />
       </div>
       <Footer />
     </div>
